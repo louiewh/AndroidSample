a805a1231cf53ab4893bc8e1d9d87398a70b3f73
louie <louie.wang.g@gmail.com>
2017-05-01 19:37:45 +0800

    view show by Fragment and add swipemenuview

---
+++ app/src/main/java/com/dawang/androidexample/view/FlowLayoutFragment.java
@@ -0,0 +1,25 @@
+package com.dawang.androidexample.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dawang.androidexample.R;
+
+/**
+ * Created by DaWang on 2017/4/30.
+ */
+
+public class FlowLayoutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_flowlayout_layout, container,false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/dawang/androidexample/view/ViewActivity.java
+++ app/src/main/java/com/dawang/androidexample/view/ViewActivity.java
@@ -1,21 +1,51 @@
 package com.dawang.androidexample.view;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Window;
 
 import com.dawang.androidexample.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by DaWang on 2017/4/13.
  */
 
-public class ViewActivity extends Activity {
+public class ViewActivity extends FragmentActivity {
+
+    private ViewPager mPager;
+    private ArrayList<Fragment> mFragment = new ArrayList<>();
+    private FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.view_activity_layout);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mFragment.add(new MyViewFragment());
+        mFragment.add(new RainViewFragment());
+        mFragment.add(new VvvFragment());
+        mFragment.add(new FlowLayoutFragment());
+
+        mFragmentManager = getSupportFragmentManager();
+        mPager.setAdapter(new FragmentPagerAdapter(mFragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragment.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragment.size();
+            }
+        });
     }
 }
