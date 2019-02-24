fe0c71a697a7f453393dc12f4140349b7504ce41
louie <louie.wang.g@gmail.com>
2019-02-24 12:11:02 +0800

    swordoffer

---
+++ app/src/test/java/com/dawang/androidsample/swordoffer/MatrixHasPath.java
@@ -0,0 +1,87 @@
+package com.dawang.androidsample.swordoffer;
+
+import org.junit.Test;
+
+public class MatrixHasPath {
+
+    @Test
+    public void test(){
+        char[] matrix = {
+                'a', 'b', 'c', 'd',
+                'f', 'a', 'c', 'k',
+                'm', 'n', 'g', 'l',
+                'o', 'p', 'x', 'y',
+        };
+
+        char[] str = {
+                'a', 'b', 'c', 'd',
+        };
+
+        System.err.print(hasPath(matrix, 4, 4, str));
+        char[] matrix2 = {
+                'A', 'B', 'C', 'E',
+                'S', 'F', 'C', 'S',
+                'A', 'D', 'E', 'E',
+        };
+
+        char[] str2 = {
+                'A', 'B', 'C', 'C', 'E', 'D'
+        };
+
+        char[] str3 = {
+                'A', 'B', 'C', 'B'
+        };
+
+        System.err.print(hasPath(matrix2, 3, 4, str3));
+    }
+
+
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+        boolean[] flags = new boolean[matrix.length];
+
+        for(int i = 0; i < rows; i++){
+
+            for(int j = 0; j < cols; j++){
+                if(visit(matrix, rows, cols, str, i, j, 0, flags)) {
+                    return true;
+                }
+            }
+        }
+
+        return  false;
+    }
+
+    boolean visit(char[] matrix, int rows, int cols, char[] str,
+                  int currentRow, int currentCol, int index, boolean[] flags){
+
+        int pos = currentRow*cols + currentCol;
+        if(currentCol < 0 || currentRow < 0
+                || currentRow >= rows || currentCol >= cols
+                || matrix[pos] != str[index]
+                || flags[pos] == true
+                || index >= matrix.length){
+            return  false;
+        }
+
+        if(index == str.length - 1) {
+            return true;
+        }
+
+        flags[pos] = true;
+        if (
+                visit(matrix, rows, cols, str, currentRow + 1, currentCol, index+1, flags) ||
+                visit(matrix, rows, cols, str, currentRow - 1, currentCol, index+1, flags) ||
+                visit(matrix, rows, cols, str, currentRow, currentCol + 1, index+1, flags) ||
+                visit(matrix, rows, cols, str, currentRow, currentCol - 1, index+1, flags)
+                )
+        {
+
+            return true;
+
+        }
+
+        flags[pos] = false;
+
+        return false;
+    }
+}
---
+++ app/src/test/java/com/dawang/androidsample/swordoffer/Permutation.java
@@ -0,0 +1,68 @@
+package com.dawang.androidsample.swordoffer;
+
+
+/*
+* 输入一个字符串,按字典序打印出该字符串中字符的所有排列。
+* 例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+*
+*/
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+
+public class Permutation {
+
+
+    @Test
+    public void test(){
+
+        ArrayList<String> list = Permutation("123");
+
+        for (String tmp : list) {
+            System.err.println(tmp);
+        }
+    }
+
+    public  ArrayList<String> Permutation(String str) {
+
+        ArrayList<String> result = new ArrayList<>();
+        if(str == null || str.length()  == 0){
+            return  result;
+        }
+
+        HashSet<String>  set = RecursionPermutation(str, 0, str.length());
+
+        result.addAll(set);
+        result.sort((o1, o2) -> o1.compareTo(o2));
+        return result;
+    }
+
+    HashSet<String> RecursionPermutation(String string, int start, int end){
+        HashSet<String> set = new HashSet<>();
+        if(start == end - 1){
+            set.add(string);
+            return set;
+        }
+
+        char[] arrays = string.toCharArray();
+        for (int i = start; i < end; i++){
+            swap(arrays, start, i);
+
+            set.addAll(RecursionPermutation(String.valueOf(arrays), start+1, end));
+
+            swap(arrays, i, start);
+        }
+
+        return set;
+    }
+
+    void swap(char[] array, int i, int j){
+        char temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
