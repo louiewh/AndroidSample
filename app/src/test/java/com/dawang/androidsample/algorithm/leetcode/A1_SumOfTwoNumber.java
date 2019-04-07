62342887ddd1e1103cd757254c84cb031c3c0421
louie <louie.wang.g@gmail.com>
2019-04-07 19:20:01 +0800

    change A1_SumOfTwoNumber

--- app/src/test/java/com/dawang/androidsample/algorithm/leetcode/A1_SumOfTwoNumber.java
+++ app/src/test/java/com/dawang/androidsample/algorithm/leetcode/A1_SumOfTwoNumber.java
@@ -2,6 +2,8 @@ package com.dawang.androidsample.algorithm.leetcode;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+
 public class A1_SumOfTwoNumber {
 
 
@@ -9,7 +11,7 @@ public class A1_SumOfTwoNumber {
     public void test(){
         int[] nums = new int[]{2, 7, 11, 15};
         int target = 9;
-        int[] result = twoSum(nums, target);
+        int[] result = twoSum2(nums, target);
 
         System.out.println(result);
     }
@@ -21,6 +23,23 @@ public class A1_SumOfTwoNumber {
                 if(nums[i] + nums[j] == target){
                     return new int[]{i, j};
                 }
+            }
+        }
+
+        return null;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int length = nums.length;
+        for(int i = 0; i< length; i++){
+            map.put(nums[i], i);
+        }
+
+        for(int i = 0; i< length; i++){
+            int temp = target - nums[i];
+            if(map.containsKey(temp) && map.get(temp) != i){
+                return new int[]{i, map.get(temp)};
             }
         }
 
